971e6ac3b39853126e1da59b98b0b04a6f890988
jstrickler <jstrickler@gmail.com>
2018-06-25 13:12:38 -0500

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+value = 45
+
+if value > 75:
+    print("wombat")
+elif value > 50:
+    print("bushbaby")
+elif value > 25:
+    print("koala")
+    print("sun bear")
+else:
+    print("kookaburra")
